572ae5cdb63a74eb5273dbc027c8ec422de1cca9
Olivier <olivier.jackson@hotmail.ca>
2020-12-03 14:03:29 -0500

    BUZZER

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,95 @@ Inclure les librairies de functions que vous voulez utiliser
 #include "LibRobus.h" // Essentielle pour utiliser RobUS
 #include <stdio.h>
 #include <math.h>
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
 
 
 
@@ -22,376 +111,48 @@ Variables globales et defines
 
 //**********************************************************************
 
-const double kp = 0.0015;// kp = 0.0015;
-const double ki = 0.00008;//  ki = 0.00008;
-const int deltat=75; //ms
-const int deltatPID=50; //ms
+// notes in the melody:
+int melody[] = {
 
-const double distanceroues =18.5; // 18.26
-const double largeurAlfred = 14.5;
+  NOTE_D5, NOTE_CS5, NOTE_B4, NOTE_FS4, NOTE_FS4, NOTE_FS4, NOTE_FS4, NOTE_FS4
+};
 
-int etape=0; 
-long int erreurTotal=0;
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
 
-//**********
-const double distanceTable=200;
-const double distanceSiege = 0;
-double distanceParcourue = 0;
-double temps = 0;
-//**********************************************************************
+  1, 1, 2, 4, 4, 4, 4, 4
+};
 
-unsigned int tempsdepause=120;
-float vitesse=0.35;
-float vitesseTourner=0.25;
-float vitesseDepart = 0.15;
-long int distanceTotal = 0;
+void setup() {
 
-//*********************************************************************
+  // iterate over the notes of the melody:
 
-int temps1 =0;
-int temps2 =0;
-int temps3 =0;
-int temps4 =0;
-int temps5 =0;
-int temps6 =0;
-int temps7 =0;
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
 
-//********************************************************************
+    // to calculate the note duration, take one second divided by the note type.
 
-float limiteQuille= 60;
-float distanceDepart=30;
-float distanceQuille;
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
 
-int micpin = A5; //pin micro
+    int noteDuration = 640 / noteDurations[thisNote];
 
-//*******************************************************************
+    tone(36, melody[thisNote], noteDuration);
 
-int alfred = 0;
-/* **********************************************************************
-Vos propres fonctions sont creees ici
-********************************************************************** */
+    // to distinguish the notes, set a minimum time between them.
 
-long int distanceEnPulse(float distance)
-{
-  // entrer distance en cm
-  return round((3200/23.94)*distance);
-}
-//**********************************************************************************************
-int pulseEnDistance(float pulse)
-{
-  return (23.94/3200)*pulse;
-}
-//**********************************************************************************************
-double pi(long int pulseAttendu, long int pulseReel)
-{
-  int erreur = pulseAttendu - pulseReel;
-  erreurTotal += erreur;
-  return kp*erreur+ki*erreurTotal;
-}
-//************************************************************************************************
-double AngleEnPulse (int angle) 
-{
-  return PI*2*distanceEnPulse(distanceroues)*angle/360.0;
-}
-//************************************************************************************************
-void avancerDistance(float vitessein, float distancein)
-{
-  // Fonction pour avancer en ligne droite avec accéleration et deccéleration.
-  // vitessein : vitesse max (entre 0 et 1)
-  // distancein : distance à parcourir (en cm)
-  long int distanceParcouru=0;
-  long int distance = distanceEnPulse(distancein);
-  unsigned long int temps =0;
-  float vitesselive = 0.35;
-  float accel=floor((6.0/475.0)*distancein-(5.0/19.0)); 
-  ENCODER_Reset(0);
-  ENCODER_Reset(1);
-  
-  while(distanceParcouru<distance)
-  {
- 
-    if(millis()-temps >= deltat)
-    {
+    // the note's duration + 30% seems to work well:
 
-      //calcul distanceParcouru
-      distanceParcouru= distanceParcouru+ENCODER_Read(1);
+    int pauseBetweenNotes = noteDuration * 1.30;
 
-      if(distancein>=100)
-      {
-        //acceleration
-        if(distanceParcouru<=(distance/(accel*4.0)))
-        {
-          vitesselive=(((-1*vitessein)/2)+(vitesseDepart/2))*cos( accel * M_PI * (distanceParcouru*4.0/distance) )+((vitessein/2)+(vitesseDepart/2));  
-        }
-        //decceleration
-        if(distanceParcouru>=(((accel*4.0)-1)*distance/(accel*4.0)))
-        {
-          vitesselive=(((-1*vitessein)/2)+(vitesseDepart/2))*cos( accel * M_PI * (distanceParcouru*4.0/distance) )+((vitessein/2)+(vitesseDepart/2));
-        }
-      }
-      //corrige la vitesse   
-      MOTOR_SetSpeed(0,vitesselive + pi(ENCODER_ReadReset(1),ENCODER_ReadReset(0)));  
-      MOTOR_SetSpeed(1,vitesselive);
-      temps = millis();
-    }
-  }//fin while
-  do
-  {
-    MOTOR_SetSpeed(0,0);
-    MOTOR_SetSpeed(1,0);
-  } while (millis()-temps<tempsdepause);
+    delay(pauseBetweenNotes);
 
+    // stop the tone playing:
 
-}
+    noTone(36);
 
-//************************************************************************************************ 
-void arreter()
-{
-  MOTOR_SetSpeed(0,0);
-  MOTOR_SetSpeed(1,0);
-}
-//************************************************************************************************ 
-void avancer(float vitessein)
-{
-  MOTOR_SetSpeed(0,vitessein);
-  MOTOR_SetSpeed(1,vitessein);
-}
-//************************************************************************************************ 
-void tournerGauche(float vitesse,int angle){
-long int distanceParcouru=0;
-
-MOTOR_SetSpeed(0,0);
-MOTOR_SetSpeed(1,vitesse);
-
-while(distanceParcouru<AngleEnPulse(angle)){
-distanceParcouru= distanceParcouru+ENCODER_ReadReset(1);   
-}
-
-MOTOR_SetSpeed(1,0);
-delay(tempsdepause);
-
-}
-//************************************************************************************************ 
-
-void tournerDroiteSurLuiMeme(float vitesseT ,int angle){
-  long int distanceParcourue = 0;
-  float angleEnPulse= AngleEnPulse(angle)/2;
-  ENCODER_Reset(0);
-  ENCODER_Reset(1);
-
-  MOTOR_SetSpeed(1,-(vitesseT));
-  MOTOR_SetSpeed(0,vitesseT);
-
-  while( distanceParcourue<angleEnPulse ){
-    delay(deltat);
-    distanceParcourue= distanceParcourue+ENCODER_Read(0);
-    MOTOR_SetSpeed(1, -(vitesseT + pi(ENCODER_ReadReset(0),-(ENCODER_ReadReset(1)))));
   }
-  MOTOR_SetSpeed(1,0);
-  MOTOR_SetSpeed(0,0);
-  delay(tempsdepause);
-}
-//**************************************************************************************************************
-
-void tournerGaucheSurLuiMeme(float vitesseT, int angle){
-  long int distanceParcourue = 0;
-  float angleEnPulse= AngleEnPulse(angle)/2;
-  ENCODER_Reset(0);
-  ENCODER_Reset(1);
-
-  MOTOR_SetSpeed(1,vitesseT);
-  MOTOR_SetSpeed(0,-(vitesseT));
-
-  while( distanceParcourue  < angleEnPulse ){
-    delay(deltat);
-    distanceParcourue= distanceParcourue+ ENCODER_Read(1);
-    MOTOR_SetSpeed(0,-(vitesseT + pi(ENCODER_ReadReset(1),-(ENCODER_ReadReset(0)))));
-  }
-  MOTOR_SetSpeed(1,0);
-  MOTOR_SetSpeed(0,0);
-  delay(tempsdepause);
-}
-//**************************************************************************************************************
-float PIDTable()
-{
-int erreur1 = 10;
-int erreur2 = 15;
-int erreur3 = 17;
-int erreur4 = 20;
-int erreur0 = 0;
-
-int A = 780; 
-int B = 780; 
-int C = 780; 
-int D = 780; 
-int E = 780; 
-int F = 780; 
-int G = 780; 
-int H = 780; 
-int erreurSuiveur ;
-
-
-      if(analogRead(6) < A) 
-      {
-        erreurSuiveur = erreur4;
-      }  
-      if(analogRead(6) >= A && analogRead(7) < B) 
-      {
-        erreurSuiveur = erreur3;  
-      }
-      if(analogRead(7) >= B && analogRead(8) < C) 
-      {
-        erreurSuiveur = erreur2;  
-      }
-      if(analogRead(8) >= C && analogRead(9) < D) 
-      {
-        erreurSuiveur = erreur1;  
-      }
-      if(analogRead(9) >= D && analogRead(10) < E) 
-      {
-        erreurSuiveur = erreur0;  
-      }
-      if(analogRead(10) >= E && analogRead(11) < F) 
-      {
-        erreurSuiveur = -erreur1;  
-      }
-      if(analogRead(11) >= F && analogRead(12) < G) 
-      {
-        erreurSuiveur = -erreur2;  
-      }
-      if(analogRead(12) >= G && analogRead(13) < H) 
-      {
-        erreurSuiveur = -erreur3;  
-      }
-      if(analogRead(13) > H) 
-      {
-        erreurSuiveur = erreur4;  
-      }
-      if(analogRead(6) >= A && analogRead(7) > B && analogRead(8) > C && analogRead(9) > D && analogRead(10) > E && analogRead(11) > F && analogRead(12) > G  && analogRead(13) > H ) 
-      {
-        erreurSuiveur = -erreur4;
-      }
-
-  return erreurSuiveur;  
- 
-}
-/************************************************************************
- * Fonction de détection des sièges
- * *********************************************************************/
-int siegeOuNon (int oui, int non)
-{
-  int valeur = 0;
-  if (millis()-temps2 >=100)
-  {
-    if (SONAR_GetRange(0)<=67.4)
-    {
-      valeur = oui;
-    }
-    else if (SONAR_GetRange(0)> 67.4)
-    {
-      valeur = non;
-    }
-  temps2 = millis();
-  }
-  return valeur;
-}
-//******************************************************************************
-int deplacementBord()
-{
-  int valeur = 0;
-  ENCODER_Reset(0);
-  ENCODER_Reset(1);
-  if (millis()- temps1 > deltatPID)
-  {
-    MOTOR_SetSpeed(1,vitesse);
-    MOTOR_SetSpeed(0, vitesse + pi(ENCODER_ReadReset(1),ENCODER_ReadReset(0))+(kp*PIDTable()));
-    temps1 = millis();
-  }
-  valeur =siegeOuNon(2,1);
-  return valeur;
-}
-//************************************************************************************
-int deplacementSiege()
-{
-  
-  int valeur = 0;
-  ENCODER_Reset(0);
-  ENCODER_Reset(1);
-
-  if (millis()- temps1 > deltatPID)
-  {
-    MOTOR_SetSpeed(1,vitesse);
-    MOTOR_SetSpeed(0, vitesse + pi(ENCODER_ReadReset(1),ENCODER_ReadReset(0))+(kp*PIDTable()));
-    temps1 = millis();
-  }
-  valeur =siegeOuNon(2,3);
-  return valeur;
-}
-//***************************************************************************
-//***********************************************************************
-//Fonctions d'initialisation (setup)
-//***********************************************************************
-
-void setup()
-{
-  pinMode(39, OUTPUT);
-  pinMode(41, OUTPUT);
-  pinMode(43, OUTPUT);
-  pinMode(A0, INPUT);
-  pinMode(A6, INPUT);
-  pinMode(A7, INPUT);
-  pinMode(A8, INPUT);
-  pinMode(A9, INPUT); 
-  pinMode(A10, INPUT);
-  pinMode(A11, INPUT);
-  pinMode(A12, INPUT);
-  pinMode(A13, INPUT);
-  BoardInit();
-  Serial.begin(9600);
 }
 
+void loop() {
 
-/******************************************************************************
-Fonctions de boucle infini (loop())
-******************************************************************************/
-void loop()
-{
-  if (digitalRead(28)==1)
-  {
-    alfred = 1;
-  }
-
-  switch (alfred)
-  {
-  case 1:
-    digitalWrite(43,LOW);
-    digitalWrite(39,HIGH);
-  // if(infrarouge voit pas le bord)
-    alfred = deplacementBord();
-  // else --> passer au case de "coinDeTable"
-
-    break;
-
-  case 2:
-    digitalWrite(39,LOW);
-    digitalWrite(41,HIGH);
-    alfred = deplacementSiege();
-    break;
-
-  case 3:
-    digitalWrite(41,LOW);
-   MOTOR_SetSpeed(1,0);
-    MOTOR_SetSpeed(0,0);
-    digitalWrite(43,HIGH);
-    //Fonction de depôt
-    alfred = 1;
-    break;
-    // pour voir si ca marche
-
-  default:
-    MOTOR_SetSpeed(0,0);
-    MOTOR_SetSpeed(1,0);
-    SERVO_Disable(0);
-    break;
-  }
+  // no need to repeat the melody.
 }
